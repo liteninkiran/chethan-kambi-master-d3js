5869989fed4706583bf951651e19c7af5b5626ce
Kiran Anand <liteninkiran@hotmail.com>
2025-03-06 15:36:58 +0000

    038 - Remove Method

---
+++ selections/modifying_elements/selection_remove/selection_remove.js
@@ -0,0 +1,10 @@
+const circle = d3.select('circle');
+const rect = d3.select('rect');
+
+const circleRemoved = circle.remove();
+const rectRemoved = rect.remove();
+
+console.log(circleRemoved);
+console.log(rectRemoved);
+
+
