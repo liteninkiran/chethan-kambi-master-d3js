a25bfc1c0357e77b2b64a9c6f49f363ceb6795bf
Kiran Anand <liteninkiran@hotmail.com>
2025-03-11 21:26:50 +0000

    068 - Logarithmic Scale

---
+++ scales/continuous/scale_log/scale_log.js
@@ -0,0 +1,12 @@
+// d3.scaleLog()
+let logScale = d3.scaleLog();
+
+// console.log(logScale(1000000)); // 6
+// console.log(logScale(0.000001)); // -6
+
+// console.log(logScale.domain()); // [1, 10]
+// console.log(logScale.range()); // [0, 1]
+
+// console.log(logScale.base()); // 10
+
+
