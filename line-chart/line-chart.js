c979f646e61f390af408719301c643ace64cd210
Kiran Anand <liteninkiran@hotmail.com>
2025-03-20 19:24:35 +0000

    Part 2

--- line-chart/line-chart.js
+++ line-chart/line-chart.js
@@ -8,55 +8,118 @@ const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleLinear().range([height, 0]);
 
 // Create the SVG element and append it to the chart container
-const svg = d3.select("#chart-container")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+const svg = d3.select('#chart-container')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
-// Create a fake dataset
-const dataset = [
-    { date: new Date("2022-01-01"), value: 200 },
-    { date: new Date("2022-02-01"), value: 250 },
-    { date: new Date("2022-03-01"), value: 180 },
-    { date: new Date("2022-04-01"), value: 300 },
-    { date: new Date("2022-05-01"), value: 280 },
-    { date: new Date("2022-06-01"), value: 220 },
-    { date: new Date("2022-07-01"), value: 300 },
-    { date: new Date("2022-08-01"), value: 450 },
-    { date: new Date("2022-09-01"), value: 280 },
-    { date: new Date("2022-10-01"), value: 600 },
-    { date: new Date("2022-11-01"), value: 780 },
-    { date: new Date("2022-12-01"), value: 320 },
-];
+// Get data
+d3.csv('jdi_data_daily.csv').then(function (dataset) {
+    // Parse the date and convert the population to a number
+    const parseDate = d3.timeParse('%Y-%m-%d');
+    dataset.forEach(d => {
+        d.date = parseDate(d.date);
+        d.population = +d.population;
+    });
 
-// Define the x and y domains
-x.domain(d3.extent(dataset, d => d.date));
-y.domain([0, d3.max(dataset, d => d.value)]);
+    dataset.sort((a, b) => a.date - b.date);
 
-// Define the x-axis
-const axisBottom = d3.axisBottom(x)
-    .ticks(d3.timeMonth.every(1))
-    .tickFormat(d3.timeFormat("%b %Y"));
+    const yMin = 75000;
 
-// Add the x-axis
-svg.append("g")
-    .attr("transform", `translate(0,${height})`)
-    .call(axisBottom);
+    // Define the x and y domains
+    x.domain(d3.extent(dataset, d => d.date));
+    y.domain([yMin, d3.max(dataset, d => d.population)]);
 
-// Add the y-axis
-svg.append("g").call(d3.axisLeft(y))
+    // Define the x-axis
+    const xAxis = d3.axisBottom(x)
+        .tickValues(x.ticks(d3.timeMonth.every(6)))
+        .tickFormat(d3.timeFormat('%b %Y'));
 
-// Create the line generator
-const line = d3.line()
-    .x(d => x(d.date))
-    .y(d => y(d.value));
+    // Add the x-axis
+    svg.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .style('font-size', '14px')
+        .call(xAxis)
+        .call(g => g.select('.domain').remove())
+        .selectAll('.tick line')
+        .style('stroke-opacity', 0);
 
-// Add the line path to the SVG element
-svg.append("path")
-    .datum(dataset)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1)
-    .attr("d", line);
+    svg.selectAll('.tick text')
+        .attr('fill', '#777');
+
+    // Define y-axis
+    const yAxis = d3.axisLeft(y)
+        .ticks((d3.max(dataset, d => d.population) - yMin) / 5000)
+        .tickFormat(d => `${(d / 1000).toFixed(0)}k`)
+        .tickSize(0)
+        .tickPadding(10);
+
+    // Add the y-axis
+    svg.append('g')
+        .style('font-size', '14px')
+        .call(yAxis)
+        .call(g => g.select('.domain').remove())
+        .selectAll('.tick text')
+        .style('fill', '#777')
+        .style('visibility', (_d, i) => i === 0 ? 'hidden' : 'visible');
+
+    // Add vertical gridlines
+    svg.selectAll('xGrid')
+        .data(x.ticks())
+        .join('line')
+        .attr('x1', d => x(d))
+        .attr('x2', d => x(d))
+        .attr('y1', 0)
+        .attr('y2', height)
+        .attr('stroke', '#e0e0e0')
+        .attr('stroke-width', .5);
+
+    // Add horizontal gridlines
+    svg.selectAll('yGrid')
+        .data(y.ticks((d3.max(dataset, d => d.population) - yMin) / 5000).slice(1))
+        .join('line')
+        .attr('x1', 0)
+        .attr('x2', width)
+        .attr('y1', d => y(d))
+        .attr('y2', d => y(d))
+        .attr('stroke', '#e0e0e0')
+        .attr('stroke-width', .5);
+
+    // Create the line generator
+
+    const line = d3.line()
+        .x(d => x(d.date))
+        .y(d => y(d.population));
+
+    // Add the line path to the SVG element
+    svg.append('path')
+        .datum(dataset)
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', 1)
+        .attr('d', line);
+
+    // Add Y-axis label
+    svg.append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 0 - margin.left)
+        .attr('x', 0 - (height / 2))
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .style('font-size', '14px')
+        .style('fill', '#777')
+        .style('font-family', 'sans-serif')
+        .text('Total Population');
+
+    // Add the chart title
+    svg.append('text')
+        .attr('class', 'chart-title')
+        .attr('x', margin.left - 115)
+        .attr('y', margin.top - 100)
+        .style('font-size', '24px')
+        .style('font-weight', 'bold')
+        .style('font-family', 'sans-serif')
+        .text('Prison Populations in the US Have Trended Upward Since Summer 2020');
+});
